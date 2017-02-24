511f52d11c81463fac25f3ec9097abc525378e4e
Carl Fuller <carl@shelliraefarm.com>
2017-02-24 17:40:53 -0500

    Added GET todos

--- server/server.js
+++ server/server.js
@@ -28,6 +28,14 @@ app.post('/todos', (req, res) => {
 
 // Read
 
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos})
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
 // Update
 
 // Delete
