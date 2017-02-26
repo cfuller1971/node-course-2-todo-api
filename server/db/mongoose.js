3c560a2da90c0508f392c4c7f5e5702a9d7beb99
Carl Fuller <carl@shelliraefarm.com>
2017-02-26 16:52:05 -0500

    Modified for mongodb at mlabs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MongoDB_URI || 'mongodb://localhost:27017/TodoApp');
 //mongoose.connect('mongodb://cfuller1971:F0rn1cat3!@ds161049.mlab.com:61049/nodetodoapi');
 
 module.exports = {mongoose};
