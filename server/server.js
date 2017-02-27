fd0dde07076142bb24cbd8ea3869c725bd3d9ebc
Carl Fuller <carl@shelliraefarm.com>
2017-02-27 14:37:20 -0500

    Broke off authenticate to middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 //mongoose.Promise = global.Promise;
 
@@ -126,6 +127,12 @@ app.post('/users', (req, res) => {
 	}).catch((e) => {
 		res.status(400).send(e);
 	});
+});
+
+// Read
+
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(req.user);
 });
 
 app.listen(port, () => {
